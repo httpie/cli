3dbb61a8cacb683a97ee48b0d6a553f52ad7a391
Jakub Roztocil <jakub@roztocil.name>
2012-03-15 00:22:29 +0100

    Added `argparse` for Python 3.1.

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ if sys.argv[-1] == 'test':
 
 
 requirements = ['requests>=0.10.4', 'Pygments>=1.4']
-if sys.version_info < (2, 7):
+if sys.version_info[:2] in ((2, 6), (3, 1)):
+    # argparse has been added in Python 3.2 / 2.7
     requirements.append('argparse>=1.2.1')
 
 
