3d11042772fe7f75008e53dc48c537fc4bbd0b1a
Jakub Roztocil <jakub@roztocil.name>
2012-04-25 02:16:10 +0200

    Fixed classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.1'
-        'Programming Language :: Python :: 3.2'
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
