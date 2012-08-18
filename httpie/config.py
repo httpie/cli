9b586b953bfe3c94adb40d0a804de3d46fca1887
Jakub Roztocil <jakub@roztocil.name>
2012-08-18 04:36:58 +0200

    Use %APPDATA% for data on Windows.

--- httpie/config.py
+++ httpie/config.py
@@ -1,6 +1,8 @@
 import os
+from requests.compat import  is_windows
 
 __author__ = 'jakub'
 
 
-CONFIG_DIR = os.path.expanduser('~/.httpie')
+CONFIG_DIR = (os.path.expanduser('~/.httpie') if not is_windows else
+              os.path.expandvars(r'%APPDATA%\\httpie'))
