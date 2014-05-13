10f7fc163b1a096ac52161a1d34a4f638adead18
Corey Farwell <coreyf@rwell.org>
2014-05-12 17:36:09 -0700

    Add supported, relevant Python version classifers

--- setup.py
+++ setup.py
@@ -69,8 +69,10 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
