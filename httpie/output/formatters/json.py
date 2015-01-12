8bd500391ba6e90a0e3657dd189b087be9c4fa03
hubbyist <hub@legrud.net>
2015-01-12 18:06:14 +0200

    use OrderedDict for json formating to preserve key ordering
    passing collections.OrderedDict to "object_pairs_hook" option of json.load function and making "sort_keys" option in json.dumps function False

--- httpie/output/formatters/json.py
+++ httpie/output/formatters/json.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import json
+import collections
 
 from httpie.plugins import FormatterPlugin
 
@@ -12,7 +13,7 @@ class JSONFormatter(FormatterPlugin):
     def format_body(self, body, mime):
         if 'json' in mime:
             try:
-                obj = json.loads(body)
+                obj = json.loads(body, object_pairs_hook=collections.OrderedDict)
             except ValueError:
                 # Invalid JSON, ignore.
                 pass
@@ -20,7 +21,7 @@ class JSONFormatter(FormatterPlugin):
                 # Indent, sort keys by name, and avoid
                 # unicode escapes to improve readability.
                 body = json.dumps(obj,
-                                  sort_keys=True,
+                                  sort_keys=False,
                                   ensure_ascii=False,
                                   indent=DEFAULT_INDENT)
         return body
