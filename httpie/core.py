96a720a4ac3bf827e22d2ffb958f687bf8b57e9d
Mihir Joshi <mihir.coldplay@gmail.com>
2015-03-28 21:44:47 -0400

    Bash completion with argcomplete
    - Made argcomplete optional. I think adding this package is causing
      build to fail. I am not sure as of now.

--- httpie/core.py
+++ httpie/core.py
@@ -27,7 +27,12 @@ from httpie.output.streams import (
     build_output_stream,
     write, write_with_colors_win_py3
 )
-import argcomplete
+HAS_ARGCOMPLETE=False
+try:
+    import argcomplete
+    HAS_ARGCOMPLETE=True
+except ImportError:
+    pass
 
 def get_exit_status(http_status, follow=False):
     """Translate HTTP status code to exit status code."""
@@ -98,7 +103,8 @@ def main(args=sys.argv[1:], env=Environment()):
     download = None
 
     try:
-        argcomplete.autocomplete(parser)
+        if HAS_ARGCOMPLETE:
+            argcomplete.autocomplete(parser)
         args = parser.parse_args(args=args, env=env)
 
         if args.download:
