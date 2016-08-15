02dc35f59cb0e7888a7b996cbfe56f790003c2d3
Rich Jones <rich@anomos.info>
2016-08-15 15:39:12 -0700

    Adds a 'httpie' alias to fix https://github.com/jkbrzt/httpie/issues/505

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
     entry_points={
         'console_scripts': [
             'http = httpie.__main__:main',
+            'httpie = httpie.__main__:main',
         ],
     },
     extras_require=extras_require,
