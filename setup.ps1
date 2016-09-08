14121e40e0d2b469c5e61fae8b08a43190d52f12
Adriano Caheté <nano.rj@gmail.com>
2016-09-08 12:03:37 -0300

    Changing encoding to utf8

--- setup.ps1
+++ setup.ps1
@@ -27,7 +27,7 @@ function Add-Path() {
 	
 	# Set the New Path
 	$NewPath=$OldPath+';'+$AddedFolder
-	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
+	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 }
 
 	
