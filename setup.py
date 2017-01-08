ee6cdf4ab3061815faa2d3e80b8da42d5a1569b9
Eugene Duboviy <eugene.dubovoy@gmail.com>
2017-01-08 16:20:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -101,6 +101,8 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
