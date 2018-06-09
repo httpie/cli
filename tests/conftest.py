749b1e2aca11e1ae3e52c8f3894cf0515c917439
Jakub Roztocil <jakub@roztocil.co>
2018-06-09 11:59:34 +0200

    Fix pytest configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,24 @@
 import pytest
-from pytest_httpbin.plugin import httpbin_ca_bundle
+from pytest_httpbin import certs
 
 
-# Make httpbin's CA trusted by default
-pytest.fixture(autouse=True)(httpbin_ca_bundle)
+@pytest.fixture(scope='function', autouse=True)
+def httpbin_add_ca_bundle(monkeypatch):
+    """
+    Make pytest-httpbin's CA trusted by default.
+
+    (Same as `httpbin_ca_bundle`, just auto-used.).
+
+    """
+    monkeypatch.setenv('REQUESTS_CA_BUNDLE', certs.where())
 
 
 @pytest.fixture(scope='function')
 def httpbin_secure_untrusted(monkeypatch, httpbin_secure):
-    """Like the `httpbin_secure` fixture, but without the
-    make-CA-trusted-by-default"""
+    """
+    Like the `httpbin_secure` fixture, but without the
+    make-CA-trusted-by-default.
+
+    """
     monkeypatch.delenv('REQUESTS_CA_BUNDLE')
     return httpbin_secure
