00050c5241b76a4a70e8cfb14de23f7619428fa7
Brad Solomon <brad.solomon.1124@gmail.com>
2020-01-20 18:05:41 -0500

    Clean up Python-version related PyPI classifiers
    - Removes 'Programming Language :: Python :: 3.5' per the
      README, which specifies 'Python version 3.6 or greater is required.'
    - Adds 'Programming Language :: Python :: 3 :: Only' in place

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Environment :: Console',
