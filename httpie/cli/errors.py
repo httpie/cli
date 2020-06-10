97995ec7c260c19096fa3a8a6b0d1282d6dbcc32
Ebonne Cabarrus <42730922+ebonnecab@users.noreply.github.com>
2020-06-10 11:49:14 -0700

    added basic template for custom error classes

---
+++ httpie/cli/errors.py
@@ -0,0 +1,24 @@
+class Error(Exception):
+    '''Base Class for custom exceptions'''
+    pass
+
+class ConnectionError(Error):
+    #TODO: Work on this class together
+    '''Exception raised for error when connecting to server'''
+    pass
+
+class BasicAuthError(Error):
+    '''Exception raised for wrong username and pw when authenticating'''
+    #TODO: may need to account for different auth types within this class
+    pass
+
+class MethodMismatchError(Error):
+    '''Exception raised when the wrong method is used with the wrong URL request'''
+    pass
+
+class LocalHostError(Error):
+    '''Exception raised when the user fails to provide a valid localhost'''
+    pass
+
+#TODO: ADD the rest of the classes including a general catch-all one for errors we may miss
+#TODO: Write tests for intended class functionctionality
