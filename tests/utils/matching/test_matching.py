a429d62fa6c064463bbffc23825705a2f0bb9cab
Jakub Roztocil <jakub@roztocil.co>
2021-01-30 17:31:40 +0100

    Cleanup

--- tests/utils/matching/test_matching.py
+++ tests/utils/matching/test_matching.py
@@ -7,11 +7,11 @@ from tests.utils import CRLF
 from tests.utils.matching import assert_output_does_not_match, assert_output_matches, Expect
 
 
-def test_assert_matches_headers_incomplete():
+def test_assert_output_matches_headers_incomplete():
     assert_output_does_not_match(f'HTTP/1.1{CRLF}', [Expect.RESPONSE_HEADERS])
 
 
-def test_assert_matches_headers_unterminated():
+def test_assert_output_matches_headers_unterminated():
     assert_output_does_not_match(
         (
             f'HTTP/1.1{CRLF}'
@@ -22,7 +22,7 @@ def test_assert_matches_headers_unterminated():
     )
 
 
-def test_assert_matches_response_headers():
+def test_assert_output_matches_response_headers():
     assert_output_matches(
         (
             f'HTTP/1.1 200 OK{CRLF}'
@@ -33,7 +33,7 @@ def test_assert_matches_response_headers():
     )
 
 
-def test_assert_matches_request_headers():
+def test_assert_output_matches_request_headers():
     assert_output_matches(
         (
             f'GET / HTTP/1.1{CRLF}'
@@ -44,7 +44,7 @@ def test_assert_matches_request_headers():
     )
 
 
-def test_assert_matches_headers_and_separator():
+def test_assert_output_matches_headers_and_separator():
     assert_output_matches(
         (
             f'HTTP/1.1{CRLF}'
@@ -56,35 +56,35 @@ def test_assert_matches_headers_and_separator():
     )
 
 
-def test_assert_matches_body_unmatched_crlf():
+def test_assert_output_matches_body_unmatched_crlf():
     assert_output_does_not_match(f'AAA{CRLF}', [Expect.BODY])
 
 
-def test_assert_matches_body_unmatched_message_separator():
+def test_assert_output_matches_body_unmatched_separator():
     assert_output_does_not_match(f'AAA{MESSAGE_SEPARATOR}', [Expect.BODY])
 
 
-def test_assert_matches_body_and_separator():
+def test_assert_output_matches_body_and_separator():
     assert_output_matches(f'AAA{MESSAGE_SEPARATOR}', [Expect.BODY, Expect.SEPARATOR])
 
 
-def test_assert_matches_body_r():
+def test_assert_output_matches_body_r():
     assert_output_matches(f'AAA\r', [Expect.BODY])
 
 
-def test_assert_matches_body_n():
+def test_assert_output_matches_body_n():
     assert_output_matches(f'AAA\n', [Expect.BODY])
 
 
-def test_assert_matches_body_r_body():
+def test_assert_output_matches_body_r_body():
     assert_output_matches(f'AAA\rBBB', [Expect.BODY])
 
 
-def test_assert_matches_body_n_body():
+def test_assert_output_matches_body_n_body():
     assert_output_matches(f'AAA\nBBB', [Expect.BODY])
 
 
-def test_assert_matches_headers_and_body():
+def test_assert_output_matches_headers_and_body():
     assert_output_matches(
         (
             f'HTTP/1.1{CRLF}'
@@ -96,7 +96,7 @@ def test_assert_matches_headers_and_body():
     )
 
 
-def test_assert_matches_headers_body_separator():
+def test_assert_output_matches_headers_with_body_and_separator():
     assert_output_matches(
         (
             f'HTTP/1.1 {CRLF}'
@@ -107,7 +107,7 @@ def test_assert_matches_headers_body_separator():
     )
 
 
-def test_assert_matches_multiple_messages():
+def test_assert_output_matches_multiple_messages():
     assert_output_matches(
         (
             f'POST / HTTP/1.1{CRLF}'
