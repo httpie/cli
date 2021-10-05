115aef92383cb8d79224d884b3b435e6de68a65d
Jakub Roztocil <jakub@roztocil.co>
2021-10-05 18:54:18 +0200

    Cleanup tests and add request body charset detection

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -13,10 +13,19 @@ from .utils import http, HTTP_OK, URL_EXAMPLE, MockEnvironment
 from .fixtures import UNICODE
 
 
-CZECH_TEXT = 'Všichni lidé jsou si rovni. Všichni lidé jsou si rovni.'
-assert len(CZECH_TEXT) > TOO_SMALL_SEQUENCE
-CZECH_TEXT_SPECIFIC_CHARSET = 'windows-1250'
-ENCODINGS = [UTF8, CZECH_TEXT_SPECIFIC_CHARSET]
+CHARSET_TEXT_PAIRS = [
+    ('big5', '卷首卷首卷首卷首卷卷首卷首卷首卷首卷首卷首卷首卷首卷首卷首卷首卷首卷首'),
+    ('windows-1250', 'Všichni lidé jsou si rovni. Všichni lidé jsou si rovni.'),
+    (UTF8, 'Všichni lidé jsou si rovni. Všichni lidé jsou si rovni.'),
+]
+
+
+def test_fixtures():
+    for charset, text in CHARSET_TEXT_PAIRS:
+        assert len(text) > TOO_SMALL_SEQUENCE
+        if charset != UTF8:
+            with pytest.raises(UnicodeDecodeError):
+                assert text != text.encode(charset).decode(UTF8)
 
 
 def test_unicode_headers(httpbin):
@@ -124,77 +133,87 @@ def test_unicode_digest_auth(httpbin):
          f'{httpbin.url}/digest-auth/auth/test/{UNICODE}')
 
 
-@pytest.mark.parametrize('encoding', ENCODINGS)
+@pytest.mark.parametrize(['charset', 'text'], CHARSET_TEXT_PAIRS)
 @responses.activate
-def test_response_body_encoding_detection_from_content(encoding):
+def test_response_body_encoding_detection_from_content(text, charset):
     responses.add(
         responses.POST,
         URL_EXAMPLE,
-        body=CZECH_TEXT.encode(encoding),
+        body=text.encode(charset),
         content_type='text/plain',
     )
     r = http('--form', 'POST', URL_EXAMPLE)
-    assert CZECH_TEXT in r
+    assert text in r
 
 
-@pytest.mark.parametrize('encoding', ENCODINGS)
+@pytest.mark.parametrize(['charset', 'text'], CHARSET_TEXT_PAIRS)
 @responses.activate
-def test_response_body_encoding_detection_from_content_type_header(encoding):
+def test_response_body_encoding_detection_from_content_type_header(charset, text):
     responses.add(
         responses.POST,
         URL_EXAMPLE,
-        body=CZECH_TEXT.encode(encoding),
-        content_type=f'text/plain; charset={encoding.upper()}',
+        body=text.encode(charset),
+        content_type=f'text/plain; charset={charset}',
     )
     r = http('--form', 'POST', URL_EXAMPLE)
-    assert CZECH_TEXT in r
+    assert text in r
 
 
-@pytest.mark.parametrize('encoding', ENCODINGS)
+@pytest.mark.parametrize(['charset', 'text'], CHARSET_TEXT_PAIRS)
 @pytest.mark.parametrize('pretty', PRETTY_MAP.keys())
 @responses.activate
-def test_response_body_encoding_detection_from_content_type_header_stream(encoding, pretty):
+def test_response_body_encoding_detection_from_content_type_header_stream(charset, text, pretty):
     responses.add(
         responses.GET,
         URL_EXAMPLE,
-        body=f'<?xml version="1.0"?>\n<c>{CZECH_TEXT}</c>'.encode(encoding),
+        body=f'<?xml version="1.0"?>\n<c>{text}</c>'.encode(charset),
         stream=True,
-        content_type=f'text/xml; charset={encoding.upper()}',
+        content_type=f'text/xml; charset={charset.upper()}',
     )
     r = http('--pretty', pretty, '--stream', URL_EXAMPLE)
-    assert CZECH_TEXT in r
+    assert text in r
 
 
+@pytest.mark.parametrize(['charset', 'text'], CHARSET_TEXT_PAIRS)
 @pytest.mark.parametrize('pretty', PRETTY_MAP.keys())
 @responses.activate
-def test_response_body_encoding_override_by_option(pretty):
+def test_response_body_encoding_override_by_option(charset, text, pretty):
     responses.add(
         responses.GET,
         URL_EXAMPLE,
-        body='卷首'.encode('big5'),
+        body=text.encode(charset),
         content_type='text/plain; charset=utf-8',
     )
     args = ('--pretty', pretty, URL_EXAMPLE)
 
-    # Encoding provided by Content-Type is incorrect, thus it should print something unreadable.
-    r = http(*args)
-    assert '卷首' not in r
-    r = http('--response-charset=big5', *args)
-    assert '卷首' in r
+    if charset != UTF8:
+        # Encoding provided by Content-Type is incorrect, thus it should print something unreadable.
+        r = http(*args)
+        assert text not in r
+    r = http('--response-charset', charset, *args)
+    assert text in r
 
 
-@pytest.mark.parametrize('encoding', ENCODINGS)
-def test_request_content_type_charset_used(encoding):
-    body_str = CZECH_TEXT
-    body_bytes = body_str.encode(encoding)
-    if encoding != UTF8:
-        with pytest.raises(UnicodeDecodeError):
-            assert body_str != body_bytes.decode()
+@pytest.mark.parametrize(['charset', 'text'], CHARSET_TEXT_PAIRS)
+def test_request_content_type_charset_used(charset, text):
+    body_bytes = text.encode(charset)
 
     r = http(
         '--offline',
         URL_EXAMPLE,
-        f'Content-Type: text/plain; charset={encoding.upper()}',
+        f'Content-Type: text/plain; charset={charset.upper()}',
         env=MockEnvironment(stdin=body_bytes, stdin_isatty=False),
     )
-    assert body_str in r
+    assert text in r
+
+
+@pytest.mark.parametrize(['charset', 'text'], CHARSET_TEXT_PAIRS)
+def test_request_charset_detected(charset, text):
+    body_bytes = text.encode(charset)
+    r = http(
+        '--offline',
+        URL_EXAMPLE,
+        f'Content-Type: text/plain',
+        env=MockEnvironment(stdin=body_bytes, stdin_isatty=False),
+    )
+    assert text in r
