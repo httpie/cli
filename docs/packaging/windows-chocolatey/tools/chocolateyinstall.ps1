344491ba8e3ca10c223e6daf5a601d5fc95cdf71
Mickaël Schoentgen <contact@tiger-222.fr>
2021-10-11 10:07:24 +0200

    Tweak the Chocolatey package installation file

--- docs/packaging/windows-chocolatey/tools/chocolateyinstall.ps1
+++ docs/packaging/windows-chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,2 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$nuspecPath =  "$(Join-Path (Split-Path -parent $toolsDir) ($env:ChocolateyPackageName + ".nuspec"))"
-[XML]$nuspec = Get-Content $nuspecPath
-$pipVersion = $nuspec.package.metadata.version
-py -m pip install "$($env:ChocolateyPackageName)==$($pipVersion)" --disable-pip-version-check
+py -m pip install $env:ChocolateyPackageName==$env:ChocolateyPackageVersion --disable-pip-version-check
