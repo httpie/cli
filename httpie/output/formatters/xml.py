7f4521b34222b5a4d0cf368af71eccdc683ad7a1
Batuhan Taskaya <isidentical@gmail.com>
2021-12-03 16:01:42 +0300

    Don't inconsistently add XML declarations

--- httpie/output/formatters/xml.py
+++ httpie/output/formatters/xml.py
@@ -1,4 +1,3 @@
-import sys
 from typing import TYPE_CHECKING, Optional
 
 from ...encoding import UTF8
@@ -8,27 +7,47 @@ if TYPE_CHECKING:
     from xml.dom.minidom import Document
 
 
+XML_DECLARATION_OPEN = '<?xml'
+XML_DECLARATION_CLOSE = '?>'
+
+
 def parse_xml(data: str) -> 'Document':
     """Parse given XML `data` string into an appropriate :class:`~xml.dom.minidom.Document` object."""
     from defusedxml.minidom import parseString
     return parseString(data)
 
 
+def parse_declaration(raw_body: str) -> Optional[str]:
+    body = raw_body.strip()
+    # XMLDecl ::= '<?xml' DECL_CONTENT '?>'
+    if body.startswith(XML_DECLARATION_OPEN):
+        end = body.find(XML_DECLARATION_CLOSE)
+        if end != -1:
+            return body[:end + len(XML_DECLARATION_CLOSE)]
+
+
 def pretty_xml(document: 'Document',
+               declaration: Optional[str] = None,
                encoding: Optional[str] = UTF8,
-               indent: int = 2,
-               standalone: Optional[bool] = None) -> str:
+               indent: int = 2) -> str:
     """Render the given :class:`~xml.dom.minidom.Document` `document` into a prettified string."""
     kwargs = {
         'encoding': encoding or UTF8,
         'indent': ' ' * indent,
     }
-    if standalone is not None and sys.version_info >= (3, 9):
-        kwargs['standalone'] = standalone
     body = document.toprettyxml(**kwargs).decode(kwargs['encoding'])
 
     # Remove blank lines automatically added by `toprettyxml()`.
-    return '\n'.join(line for line in body.splitlines() if line.strip())
+    lines = [line for line in body.splitlines() if line.strip()]
+
+    # xml.dom automatically adds the declaration, even if
+    # it is not present in the actual body. Remove it.
+    if len(lines) >= 1 and parse_declaration(lines[0]):
+        lines.pop(0)
+        if declaration:
+            lines.insert(0, declaration)
+
+    return '\n'.join(lines)
 
 
 class XMLFormatter(FormatterPlugin):
@@ -44,6 +63,7 @@ class XMLFormatter(FormatterPlugin):
         from xml.parsers.expat import ExpatError
         from defusedxml.common import DefusedXmlException
 
+        declaration = parse_declaration(body)
         try:
             parsed_body = parse_xml(body)
         except ExpatError:
@@ -54,6 +74,6 @@ class XMLFormatter(FormatterPlugin):
             body = pretty_xml(parsed_body,
                               encoding=parsed_body.encoding,
                               indent=self.format_options['xml']['indent'],
-                              standalone=parsed_body.standalone)
+                              declaration=declaration)
 
         return body
