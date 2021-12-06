ec48473b1ec08cde661b5e81814e3aebf4c49726
Batuhan Taskaya <isidentical@gmail.com>
2021-12-06 18:00:31 +0300

    Add initial benchmarking infrastructure

---
+++ extras/profiling/benchmarks.py
@@ -0,0 +1,90 @@
+# http_bench: HTTPie benchmarking utilities
+from __future__ import annotations
+
+import os
+import sys
+import tempfile
+from dataclasses import dataclass, field
+from datetime import datetime
+from functools import partial
+from typing import Callable, ClassVar, Dict, List
+
+import pyperf
+
+BenchFunction = Callable[['Context'], None]
+
+
+@dataclass
+class Context:
+    benchmarks: ClassVar[List[BaseRunner]] = []
+    runner: pyperf.Runner = field(default_factory=pyperf.Runner)
+
+    def run(self) -> pyperf.BenchmarkSuite:
+        results = [benchmark.run(self) for benchmark in self.benchmarks]
+        return pyperf.BenchmarkSuite(results)
+
+    @property
+    def cmd(self) -> List[str]:
+        http = os.path.join(os.path.dirname(sys.executable), 'http')
+        assert os.path.exists(http)
+        return [sys.executable, http]
+
+
+@dataclass
+class BaseRunner:
+    category: str
+    title: str
+
+    def __post_init__(self):
+        Context.benchmarks.append(self)
+
+    def run(self, context: Context) -> pyperf.Benchmark:
+        raise NotImplementedError
+
+    @property
+    def name(self) -> str:
+        return f'{self.category}-{self.title}'
+
+
+@dataclass
+class FunctionRunner(BaseRunner):
+    func: BenchFunction
+
+    @classmethod
+    def from_func(cls, *args) -> Callable[[BenchFunction], BenchFunction]:
+        def wrapper(func: BenchFunction) -> BenchFunction:
+            cls(*args, func=func)
+            return func
+
+        return wrapper
+
+    def run(self, context: Context) -> pyperf.Benchmark:
+        return context.runner.bench_func(self.name, self.func, context)
+
+
+@dataclass
+class CommandRunner(BaseRunner):
+    args: List[str]
+
+    def run(self, context: Context) -> pyperf.Benchmark:
+        return context.runner.bench_command(
+            self.name, [*context.cmd, *self.args]
+        )
+
+
+CommandRunner('startup', 'version', ['--version'])
+CommandRunner('startup', 'offline request', ['--offline', 'pie.dev/get'])
+
+
+def main() -> None:
+    # PyPerf will bring it's own argument parser, so configure the script.
+    sys.argv.extend(
+        ['--worker', '--loops=1', '--warmup=2', '--values=5', '--processes=2']
+    )
+
+    context = Context()
+    context.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ extras/profiling/run.py
@@ -0,0 +1,152 @@
+import subprocess
+import tempfile
+import venv
+from argparse import ArgumentParser
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Dict, Generator, Iterable, List, NamedTuple, Optional
+
+BENCHMARK_SCRIPT = Path(__file__).parent / 'benchmarks.py'
+CURRENT_REPO = Path(__file__).parent.parent.parent
+
+GITHUB_URL = 'https://github.com/httpie/httpie.git'
+TARGET_BRANCH = 'master'
+
+ADDITIONAL_DEPS = [
+    'pyOpenSSL',
+]
+
+
+def call(*args, **kwargs):
+    kwargs.setdefault('stdout', subprocess.DEVNULL)
+    return subprocess.check_call(*args, **kwargs)
+
+
+class Environment(NamedTuple):
+    repo_url: str
+    branch: Optional[str] = None
+    dependencies: Iterable[str] = ()
+
+
+@contextmanager
+def on_repo(env: Environment) -> Generator[Path, None, None]:
+    with tempfile.TemporaryDirectory() as directory_path:
+        directory = Path(directory_path)
+
+        # Clone the repo
+        repo_path = directory / 'httpie'
+        call(
+            ['git', 'clone', env.repo_url, repo_path],
+            stderr=subprocess.DEVNULL,
+        )
+
+        if env.branch is not None:
+            call(
+                ['git', 'checkout', env.branch],
+                cwd=repo_path,
+                stderr=subprocess.DEVNULL,
+            )
+
+        # Prepare the environment
+        venv_path = directory / '.venv'
+        venv.create(venv_path, with_pip=True)
+
+        python = venv_path / 'bin' / 'python'
+        call(
+            [
+                python,
+                '-m',
+                'pip',
+                'install',
+                'wheel',
+                'pyperf',
+                *env.dependencies,
+            ]
+        )
+        call([python, 'setup.py', 'bdist_wheel'], cwd=repo_path)
+
+        distribution_path = next((repo_path / 'dist').iterdir())
+        call(
+            [python, '-m', 'pip', 'install', distribution_path], cwd=repo_path
+        )
+
+        http = venv_path / 'bin' / 'http'
+        yield python
+
+
+def run(configs: List[Dict[str, Environment]]) -> None:
+    result_directory = Path(tempfile.mkdtemp())
+    result_outputs = {}
+
+    current = 1
+    total = len(configs) * 2
+
+    for config in configs:
+        for env_name, env in config.items():
+            print(
+                f'Iteration: {env_name} ({current}/{total})' + ' ' * 10,
+                end='\r',
+                flush=True,
+            )
+            with on_repo(env) as python:
+                call(
+                    [python, BENCHMARK_SCRIPT, '-o', env_name],
+                    cwd=result_directory,
+                )
+            current += 1
+
+        env_names = tuple(config.keys())
+        result_outputs[env_names] = subprocess.check_output(
+            ['pyperf', 'compare_to', *env_names],
+            cwd=result_directory,
+            text=True,
+        )
+    print()
+
+    for env_names, result in result_outputs.items():
+        print(' -> '.join(env_names))
+        print(result)
+
+    print('Results are available at:', result_directory)
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument('--local-repo', default=CURRENT_REPO)
+    parser.add_argument('--local-branch', default=None)
+    parser.add_argument('--target-repo', default=CURRENT_REPO)
+    parser.add_argument('--target-branch', default=TARGET_BRANCH)
+    parser.add_argument(
+        '--fresh',
+        action='store_const',
+        const=GITHUB_URL,
+        dest='target_repo',
+        help='Clone the target repo from upstream GitHub URL',
+    )
+    parser.add_argument(
+        '--complex',
+        action='store_true',
+        help='Add a second run, with a complex python environment.',
+    )
+    options = parser.parse_args()
+
+    configs = []
+
+    base_config = {
+        'remote': Environment(options.target_repo, options.target_branch),
+        'local': Environment(options.local_repo, options.local_branch),
+    }
+    configs.append(base_config)
+
+    if options.complex:
+        complex_config = {
+            env_name + '-complex': env._replace(dependencies=ADDITIONAL_DEPS)
+            for env_name, env in base_config.items()
+        }
+        configs.append(complex_config)
+
+    run(configs)
+
+
+if __name__ == '__main__':
+    main()
