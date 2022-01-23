12d3b5217c252dba489dc7f2dfc2be95af0a3a9a
Jakub Roztocil <jakub@roztocil.co>
2022-01-23 13:26:16 +0100

    Cleanup tests

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -2,16 +2,16 @@ from httpie.models import ELAPSED_TIME_LABEL
 from .utils import http, MockEnvironment
 
 
-def test_meta_elapsed_time(httpbin, monkeypatch):
+def test_meta_elapsed_time(httpbin):
     r = http('--meta', httpbin + '/delay/1')
     assert f'{ELAPSED_TIME_LABEL}: 1.' in r
 
 
-def test_meta_elapsed_time_colors_pie_style(httpbin, monkeypatch):
-    r = http('--style=fruity', '--meta', httpbin + '/get', env=MockEnvironment(colors=256))
+def test_meta_elapsed_time_colors_pie_style(httpbin):
+    r = http('--style=pie-dark', '--meta', httpbin + '/get', env=MockEnvironment(colors=256))
     assert ELAPSED_TIME_LABEL in r
 
 
-def test_meta_elapsed_time_colors_non_pie_style(httpbin, monkeypatch):
-    r = http('--style=pie-dark', '--meta', httpbin + '/get', env=MockEnvironment(colors=256))
+def test_meta_elapsed_time_colors_non_pie_style(httpbin):
+    r = http('--style=fruity', '--meta', httpbin + '/get', env=MockEnvironment(colors=256))
     assert ELAPSED_TIME_LABEL in r
