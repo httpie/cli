338f27e86ee0e7c21d715be4aee2385f6899f55e
Batuhan Taskaya <isidentical@gmail.com>
2022-03-15 12:37:36 +0300

    Man page deployment & isolation. (#1325)
    * Remove all unsorted usages in the CLI docs
    
    * Implement isolated mode for man page generation
    
    * Add a CI job for autogenerated files
    
    * Distribute man pages through PyPI

--- setup.py
+++ setup.py
@@ -111,4 +111,8 @@ setup(
         'Documentation': 'https://httpie.io/docs',
         'Online Demo': 'https://httpie.io/run',
     },
+    data_files=[
+        ('share/man/man1', ['extras/man/http.1']),
+        ('share/man/man1', ['extras/man/https.1']),
+    ]
 )
