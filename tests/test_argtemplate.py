c74aabb9d5aed99935a5646f6b968224788bc102
editf <edit.flores.skola@gmail.com>
2023-03-02 00:48:37 +0100

    Add one unit test for editing a template
    This adds a unit test that checks that a value is correctly updated
    when modifying a certain parameter, and leaving the other values
    unchanged.

--- tests/test_argtemplate.py
+++ tests/test_argtemplate.py
@@ -58,3 +58,30 @@ class TestStoreTemplate:
             template_data = template['data']
             assert template_data['param1'] == 'value1'
             assert template_data['param2'] == 'value2'
+
+class TestEditTemplate:
+    def test_edit_template_update_value(self):
+        """
+        Tests that the edit_json_template function can correctly update a value
+        """
+        with tempfile.NamedTemporaryFile('w+', delete=False) as temp_fp:
+            httpie.cli.argtemplate.TEMPLATE_FILE = temp_fp.name
+
+            command = 'http template test_template GET https://catfact.ninja/fact param1=value1 param2=value2'
+            args = command.split()
+
+            httpie.cli.argtemplate.store_json_template(args[2:])
+
+            httpie.cli.argtemplate.edit_json_template(['test_template', 'param1', 'newvalue1'])
+
+            stored_templates = json.load(temp_fp)
+            assert stored_templates[args[2]] is not None
+
+            template = stored_templates[args[2]]
+            assert template['method'] == 'GET'
+            assert template['url'] == args[4]
+            assert template['data'] is not None
+
+            template_data = template['data']
+            assert template_data['param1'] == 'newvalue1'
+            assert template_data['param2'] == 'value2'
