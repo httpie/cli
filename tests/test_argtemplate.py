9b6cd92d84d91ae3ffe7b8cd373afad73e6d3ad0
editf <edit.flores.skola@gmail.com>
2023-03-06 17:47:33 +0100

    Add two more unit tests for store_template
    This includes one test for overwriting an already existing template,
    which passes, and one test for storing a template with an invalid method
    parameter, which currently fails (see #11).

--- tests/test_argtemplate.py
+++ tests/test_argtemplate.py
@@ -59,6 +59,50 @@ class TestStoreTemplate:
             assert template_data['param1'] == 'value1'
             assert template_data['param2'] == 'value2'
 
+    def test_store_template_overwrites_old_template_same_name(self):
+        """
+        Tests that storing a template with the same name as an already existing template will overwrite the existing template
+        """
+        with tempfile.NamedTemporaryFile('w+', delete=False) as temp_fp:
+
+            httpie.cli.argtemplate.TEMPLATE_FILE = temp_fp.name
+
+            command = 'http template test_template GET https://catfact.ninja/fact param1=value1 param2=value2'
+            args = command.split()
+
+            httpie.cli.argtemplate.store_json_template(args[2:])
+
+            old_stored_templates = json.load(temp_fp)
+
+            command = 'http template test_template POST https://catfact.ninja/fact param1=value2 param2=value1'
+            args = command.split()
+            httpie.cli.argtemplate.store_json_template(args[2:])
+
+            temp_fp.seek(0)
+            new_stored_templates = json.load(temp_fp)
+
+            assert len(old_stored_templates) == len(new_stored_templates)
+            assert new_stored_templates[args[2]]['method'] == 'POST'
+            assert new_stored_templates[args[2]]['data']['param1'] == 'value2'
+            assert new_stored_templates[args[2]]['data']['param2'] == 'value1'
+
+    def test_store_template_invalid_method(self):
+        """
+        Tests that storing a template with an invalid method argument leads to the template being saved without a method parameter
+        """
+        with tempfile.NamedTemporaryFile('w+', delete=False) as temp_fp:
+            httpie.cli.argtemplate.TEMPLATE_FILE = temp_fp.name
+
+            command = 'http template test_template NOT_VALID_METHOD https://catfact.ninja/fact param1=value1 param2=value2'
+            args = command.split()
+
+            httpie.cli.argtemplate.store_json_template(args[2:])
+
+            stored_templates = json.load(temp_fp)
+            print(stored_templates)
+            template = stored_templates[args[2]]
+            assert template['method'] is None
+
 class TestEditTemplate:
     def test_edit_template_update_value(self):
         """
