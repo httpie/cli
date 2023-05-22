2da955fb0619e18078c56b5bec9dae8c7d99d0e3
Jakub Roztocil <jakub@roztocil.co>
2023-05-22 11:56:30 -0700

    Man page clean-up (#1508)
    Ensure we don’t include dynamic content in the static man pages.

--- httpie/output/ui/man_pages.py
+++ httpie/output/ui/man_pages.py
@@ -4,23 +4,22 @@ import subprocess
 import os
 from httpie.context import Environment
 
+
 MAN_COMMAND = 'man'
 NO_MAN_PAGES = os.getenv('HTTPIE_NO_MAN_PAGES', False)
 
-# On some systems, HTTP(n) might exist but we are only
-# interested in HTTP(1).
-#
-# For more information on man page sections: https://unix.stackexchange.com/a/138643
-
+# On some systems, HTTP(n) might exist, but we are only interested in HTTP(1).
+# For more information on man page sections: <https://unix.stackexchange.com/a/138643>
 MAN_PAGE_SECTION = '1'
 
 
 def is_available(program: str) -> bool:
-    """Check whether HTTPie's man pages are available in this system."""
+    """
+    Check whether `program`'s man pages are available on this system.
 
+    """
     if NO_MAN_PAGES or os.system == 'nt':
         return False
-
     try:
         process = subprocess.run(
             [MAN_COMMAND, MAN_PAGE_SECTION, program],
@@ -29,7 +28,7 @@ def is_available(program: str) -> bool:
             stderr=subprocess.DEVNULL
         )
     except Exception:
-        # There might be some errors outside of the process, e.g
+        # There might be some errors outside the process, e.g
         # a permission error to execute something that is not an
         # executable.
         return False
@@ -38,8 +37,10 @@ def is_available(program: str) -> bool:
 
 
 def display_for(env: Environment, program: str) -> None:
-    """Display the man page for the given command (http/https)."""
+    """
+    Open the system man page for the given command (http/https/httpie).
 
+    """
     subprocess.run(
         [MAN_COMMAND, MAN_PAGE_SECTION, program],
         stdout=env.stdout,
